74298c64d3d8250acd5f1db9540f4e92284f97df
harus0313 <s1130087@gmail.com>
2021-07-06 22:34:41 +0900

    20210706

---
+++ DataModel/pagelist.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace GenPageList.DataModel
+{
+    public class pagelist
+    {
+        public class Data
+        {
+            public string url {get; set;}
+            public string title {get; set;}
+        }
+
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using AngleSharp;
+using AngleSharp.Html.Parser;
+using System.Net;
+using System.Threading.Tasks;
+
+using System;
 
 namespace GenPageList
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            DataModel.pagelist.Data data = new DataModel.pagelist.Data();
+            data.url = "https://usefuledge.com/csharp-json.html";
+
+            Console.WriteLine(await GetWebTitle(data.url));
+
+        }
+
+
+        static async Task<string> GetWebTitle(string url)
+        {
+            WebClient wc = new WebClient();
+            try
+            {
+                string htmldocs = wc.DownloadString(url);
+
+                var config = Configuration.Default;
+                var context = BrowsingContext.New(config);
+                var document = await context.OpenAsync(req => req.Content(htmldocs));
+
+                //Console.WriteLine(document.Title);
+
+                return document.Title;
+
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
         }
     }
 }
