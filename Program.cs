48b8f3544f0776bca835bd368cc014bce49a228b
harus0313 <s1130087@gmail.com>
2021-07-06 22:17:38 +0900

    20210706

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenPageList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
