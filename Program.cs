72e55ecdc4890af9822e590f79a0de75004bbe2c
harus0313 <s1130087@gmail.com>
2021-07-13 21:22:45 +0900

    20210713

--- Program.cs
+++ Program.cs
@@ -13,6 +13,14 @@ namespace GenPageList
         static async Task Main(string[] args)
         {
 
+            //filelist.dbから処理対象URL一覧を取得する
+            //処理フラグが1のURLのページタイトルを取得する（404チェック含む）
+            //ページが存在するもののサイトマップ(markdown)を出力する
+            //markdownからhtmlに変換する
+            //htmlをsitemap.cshtmlへ出力する
+            //usefuledge にてコンパイル
+
+
             //await DataModel.pagelist.UpdateTite();
             //DataModel.pagelist.OutputSitemapMd();
             DataModel.pagelist.MarkdownToHTML();
